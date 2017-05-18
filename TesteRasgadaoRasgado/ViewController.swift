32226d3a724c31c34986c8de288daa23315f89f1
Luan Costa <luancosta@MacBook-Pro-de-Luan-Costa.local>
2017-05-18 15:11:51 -0300

    teste meu Deus

--- TesteRasgadaoRasgado/ViewController.swift
+++ TesteRasgadaoRasgado/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let teste = "Hue"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
