a585aeec84674d8fb49d0229225e9d01514e9dfb
Luan Costa <luancosta@MacBook-Pro-de-Luan-Costa.local>
2017-05-18 11:34:59 -0300

    Initial Commit

---
+++ TesteRasgadaoRasgado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteRasgadaoRasgado
+//
+//  Created by Luan Costa on 18/05/17.
+//  Copyright © 2017 Luan Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
